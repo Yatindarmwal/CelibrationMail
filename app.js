ce8477b281abadbd0882f8e15d8700d9d628fdfb
Yatin Darmwal <yatin@infeedo.com>
2019-11-04 22:46:56 +0530

    api setup

--- app.js
+++ app.js
@@ -3,8 +3,9 @@
 const express = require('express');
 const celibration_mail_cron = require('./background_process/celibration_mail_cron');
 const app = express();
-const port = 3000;
+const port = 3001;
 app.use(require('./routes/website_routes'));
+app.use(require('./routes/api_routes'));
 app.listen(port, () => console.log(`Celibration Mail app running on port ${port}!`));
 
 
